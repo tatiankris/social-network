0adbd16e23a151f64b733056042ca2b9c018c29c
tatiankris <tatiankris@mail.ru>
2021-09-14 18:54:14 +0200

    add NavLink less-20

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from "./Navbar.module.css"
 
 function  Navbar () {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a href={'/profile'}>Profile</a>
+                <NavLink to={'/profile'} activeClassName={s.active}>Profile</NavLink>
             </div>
             <div className={`${s.item} ${s.active}`}>
-                <a href={'/dialogs'}>Messages</a>
+                <NavLink to={'/dialogs'} activeClassName={s.active}>Messages</NavLink>
             </div>
             <div className={s.item}>
-                <a href={'/news'}>News</a>
+                <NavLink to={'/news'} activeClassName={s.active}>News</NavLink>
             </div>
             <div className={s.item}>
-                <a href={'/music'}>Music</a>
+                <NavLink to={'/music'} activeClassName={s.active}>Music</NavLink>
             </div>
             <div className={s.item}>
-                <a href={'/settings'}>Settings</a>
+                <NavLink to={'/settings'} activeClassName={s.active}>Settings</NavLink>
             </div>
         </nav>
     )
