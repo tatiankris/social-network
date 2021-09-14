63adf9bac242748e4a09247f98b18b7a4991310b
tatiankris <tatiankris@mail.ru>
2021-09-15 00:12:04 +0200

    added Dialogs styles less-21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -7,8 +7,32 @@ type DialogPropsType = {
 
 const Dialogs = (props: DialogPropsType) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Dimych
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    Andrew
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.massage}>Hi</div>
+                <div className={s.massage}>How is your it?</div>
+                <div className={s.massage}>Goodbye</div>
+            </div>
         </div>
     )
 }
